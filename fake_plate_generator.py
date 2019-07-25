c1cc73ea9db0ab18894ffaa343242ac84643409d
Bailey Yu <bailey.yu@yahoo.com>
2019-07-25 11:37:17 -0700

    added more options

--- fake_plate_generator.py
+++ fake_plate_generator.py
@@ -17,6 +17,9 @@ output_dir = args.img_dir
 resample_range = args.resample 
 gaussian_range = args.gaussian 
 noise_range = args.noise
+rank_blur = args.rank_blur
+brightness = args.brightness
+motion_blur = args.motion_blur
 number_dir = fake_resource_dir + "/numbers/"
 letter_dir = fake_resource_dir + "/letters/" 
 plate_dir = fake_resource_dir + "/plate_background_use/"
@@ -154,4 +157,7 @@ if __name__ == "__main__":
         plate = jittering_scale(plate)
         #plate = invertColor(plate)
         #plate = perspectiveTransform(plate)
+        plate = random_rank_blur(plate,rank_blur)
+        plate = random_motion_blur(plate,motion_blur)
+        plate = random_brightness(plate, brightness)
         save_random_img(output_dir, plate)
